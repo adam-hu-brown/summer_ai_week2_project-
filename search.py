83ffc5cd88bdf75713a9692bdfaaa7e0b4ae3dc8
Adam Hu <adamhu@Adams-MacBook-Air-2.local>
2023-07-21 21:28:06 -0400

    added dfs search

--- search.py
+++ search.py
@@ -86,7 +86,25 @@ def depthFirstSearch(problem: SearchProblem):
     print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
     print("Start's successors:", problem.getSuccessors(problem.getStartState()))
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    result = dfs_search(problem)
+    print("DFS Result is:", result)
+    return result
+
+def dfs_search(problem):
+    frontier = util.Stack()
+    frontier.push([problem.getStartState(), []])
+    visited = util.Queue()
+    goal_found = False
+    while not goal_found:
+        node = frontier.pop()
+        if problem.isGoalState(node[0]):
+            goal_found = True
+        visited.push(node[0])
+        for child in problem.getSuccessors(node[0]):
+            if not(visited.has(child[0])):
+                frontier.push([child[0], node[1] + [child[1]]])
+    return node[1]
+    # This should be the result.... ['West', 'West', 'West', 'West', 'South', 'South', 'East', 'South', 'South', 'West']
 
 def breadthFirstSearch(problem: SearchProblem):
     """Search the shallowest nodes in the search tree first."""
